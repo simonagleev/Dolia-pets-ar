46b7b538eb8290be49221d41c4a06007318b3bf8
Simon <62551180+simonagleev@users.noreply.github.com>
2023-01-14 01:22:23 +0300

    stable. floor detection, scaling, rotation to the camera

--- main.js
+++ main.js
@@ -44,52 +44,98 @@ document.addEventListener('DOMContentLoaded', () => {
         const renderer = new THREE.WebGLRenderer({antialias: true, alpha: true })
         const camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 20)
 
+        const clock = new THREE.Clock()
+
         renderer.setSize(window.innerWidth, window.innerHeight)
         renderer.setPixelRatio(window.devicePixelRatio)
         document.body.appendChild(renderer.domElement)
 
         const light = new THREE.HemisphereLight(0XFFFFFF, 0xbbbbff, 1)
         scene.add(light)
+        
 
-        if (pet) {
-            // scene.add(pet)
-            pet.lookAt(camera.position)
-        }
+        // Helping circle 
+        const circleGeometry = new THREE.RingGeometry(0.15, 0.2, 32);
+        circleGeometry.rotateX(-Math.PI / 2)
+        const circleMaterial = new THREE.MeshBasicMaterial({color: '#FFF2FD'})
+        const circle = new THREE.Mesh(circleGeometry, circleMaterial)
+        circle.matrixAutoUpdate = false;
+        circle.visible = false;
+        scene.add(circle)
 
         // Controllers
         const controller = renderer.xr.getController(0)
         scene.add(controller)
         
 
+        // EVENTS
         controller.addEventListener('select', () => {
             textPlacePet.style.display === 'block' ? textPlacePet.style.display = 'none' : null
 
             if (!scene.getObjectByName('pet')) {
-                pet.position.applyMatrix4(controller.matrixWorld)
-                // pet.quaternion.setFromRotationMatrix(controller.matrixWorld)
+                if (circle.visible === true) {
+                    pet.scale.set(1, 1, 1)
+                    pet.position.setFromMatrixPosition(circle.matrix)
+                } else {
+                    pet.position.applyMatrix4(controller.matrixWorld)
+                }
+                pet.quaternion.setFromRotationMatrix(controller.matrixWorld)
                 scene.add(pet)
 
             } else {
                 pet.position.set(0,0,0)
-                pet.position.applyMatrix4(controller.matrixWorld)
-                // pet.quaternion.setFromRotationMatrix(controller.matrixWorld)
-
+                if (circle.visible === true) {
+                    pet.scale.set(1, 1, 1)
+                    pet.position.setFromMatrixPosition(circle.matrix)
+                } else {
+                    pet.position.applyMatrix4(controller.matrixWorld)
+                }
+                // pet.position.setFromMatrixPosition(circle.matrix)
+                pet.quaternion.setFromRotationMatrix(controller.matrixWorld)
             }
+        })
+
+        renderer.xr.addEventListener('sessionstart', async() => {
+            const session = renderer.xr.getSession()
+            const viewerReferenceSpace = await session.requestReferenceSpace('viewer')
+            const hitTestSuorce = await session.requestHitTestSource({space: viewerReferenceSpace})
+
+            renderer.setAnimationLoop((timeStamp, frame) => {
+                if(!frame) return
+                const hitTestResult = frame.getHitTestResults(hitTestSuorce)
+                mixer.update(clock.getDelta())
+                pet ? pet.lookAt(camera.position) : null
+
+                if(hitTestResult.length > 0) {
+                    const referenceSpace = renderer.xr.getReferenceSpace()
+                    const hit = hitTestResult[0]
+                    const hitPose = hit.getPose(referenceSpace)
+
+                    circle.visible = true
+                    circle.matrix.fromArray(hitPose.transform.matrix)
+                } else {
+                    circle.visible = false
+                }
+
+                renderer.render(scene, camera)
+            })
+        })
+
+        renderer.xr.addEventListener('sessionend', async() => {
+            
         })
 
         // Start and end AR
         let currentSection = null;
 
         const start = async () => {
-            currentSection = await navigator.xr.requestSession("immersive-ar", { optionalFeatures: ['dom-overlay'], domOverlay: { root: document.body } })
+            currentSection = await navigator.xr.requestSession("immersive-ar", { requiredFeatures: ['hit-test'], optionalFeatures: ['dom-overlay'], domOverlay: { root: document.body } })
             renderer.xr.enabled = true
             renderer.xr.setReferenceSpaceType('local')
             await renderer.xr.setSession(currentSection)
 
             textPlacePet.style.display = 'block'
             arButton.textContent = 'End'
-
-            const clock = new THREE.Clock()
 
             renderer.setAnimationLoop(() => [
                 renderer.render(scene, camera),
