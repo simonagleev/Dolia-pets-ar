5f67dfc963bac53a1f5e2d7480c3b8dd9187e784
Simon <62551180+simonagleev@users.noreply.github.com>
2023-01-15 23:44:59 +0300

    title and comments

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
 // HTML
 const textPlacePet = document.querySelector('.place-pet')
 
-// Loading
+// Loaders
 const loadingManager = new THREE.LoadingManager()
 const loader = new GLTFLoader(loadingManager)
 
